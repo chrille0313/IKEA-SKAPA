48cfa6023e2a2f40551f3ec0ba0e53fe79290ee7
Christian <chrille_0313@hotmail.com>
2024-10-02 13:58:56 +0200

    feat: add figma demo page

---
+++ apps/ikea-photo-creator/src/components/FigmaDemo/FigmaDemo.tsx
@@ -0,0 +1,13 @@
+export default function FigmaDemo() {
+  return (
+    <iframe
+      allowFullScreen
+      width="100%"
+      height="89%"
+      style={{
+        border: 'none'
+      }}
+      src="https://embed.figma.com/proto/1VcM61qP5IhwQVXA7uE63a/Ikea-Prototype?node-id=1-3336&embed-host=share"
+    />
+  );
+}
