4636c3c70a30ed97898857628131fb7beb021ccc
Christian <chrille_0313@hotmail.com>
2024-10-04 00:01:57 +0200

    feat: create contact section

---
+++ apps/ikea-photo-creator/src/components/ContactForm/ContactForm.tsx
@@ -0,0 +1,83 @@
+'use client';
+
+import SendIcon from '@mui/icons-material/Send';
+import { Autocomplete, Box, Button, Stack, StackProps, TextField } from '@mui/material';
+import NextLink from 'next/link';
+import { useState } from 'react';
+
+const contactInformation = [
+  {
+    name: 'Christian Mattsson',
+    email: 'chrille_0313@hotmail.com'
+  },
+  {
+    name: 'Leopold Wahlbeck',
+    email: 'leopoldwahlbeck@gmail.com'
+  },
+  {
+    name: 'Ida Åberg',
+    email: ''
+  },
+  {
+    name: 'Senja Kortesaari',
+    email: ''
+  },
+  {
+    name: 'Lukas Hén',
+    email: ''
+  },
+  {
+    name: 'Emil Andersson',
+    email: ''
+  }
+];
+
+export default function ContactForm(props: StackProps) {
+  const [contact, setContact] = useState(contactInformation);
+  const [message, setMessage] = useState('');
+
+  function getMailToLink() {
+    return `mailto:${contact.map((contact) => contact.email).join(',')}?body=${message}`;
+  }
+
+  return (
+    <Stack gap="1rem" {...props}>
+      <Autocomplete
+        id="contact"
+        multiple
+        limitTags={1}
+        options={contactInformation}
+        getOptionLabel={(option) => option.name}
+        value={contact}
+        defaultValue={contactInformation}
+        onChange={(event, newValue) => setContact(newValue)}
+        renderInput={(params) => <TextField {...params} label="Contact" />}
+        disablePortal
+        disableListWrap
+        disableCloseOnSelect
+        filterSelectedOptions
+      />
+      <TextField
+        id="message"
+        label="Message"
+        variant="outlined"
+        rows={10}
+        multiline
+        value={message}
+        onChange={(e) => setMessage(e.target.value as string)}
+      />
+      <Box display="flex" justifyContent="center">
+        <Button
+          variant="contained"
+          size="large"
+          style={{ borderRadius: 50 }}
+          LinkComponent={NextLink}
+          endIcon={<SendIcon />}
+          href={getMailToLink()}
+        >
+          Send
+        </Button>
+      </Box>
+    </Stack>
+  );
+}
