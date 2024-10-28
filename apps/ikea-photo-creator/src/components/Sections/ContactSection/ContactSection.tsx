7858ed84480a690514024cbcb0697c2e4cb6d5d1
Christian <chrille_0313@hotmail.com>
2024-10-28 03:46:38 +0100

    feat: make components responsive

--- apps/ikea-photo-creator/src/components/Sections/ContactSection/ContactSection.tsx
+++ apps/ikea-photo-creator/src/components/Sections/ContactSection/ContactSection.tsx
@@ -5,17 +5,17 @@ import SectionTitle from '@/components/Sections/SectionTitle';
 import SectionHeader from '@/components/Sections/SectionHeader';
 import ContactForm from '@/components/ContactForm';
 import Image from 'next/image';
-import mailIcon from '@/../public/static/images/graphics/undraw_mailbox_re_dvds.svg';
+import MailIcon from '@/../public/static/images/graphics/undraw_mailbox_re_dvds.svg';
 
 export default function ContactSection() {
   return (
     <Container>
       <Stack direction="row" gap="5rem" alignItems="center" justifyContent="center">
-        <Box maxWidth="450px" marginTop="5rem">
-          <Image src={mailIcon} alt="mail icon" />
+        <Box width="450px" marginTop="5rem" display={{ xs: 'none', md: 'flex' }}>
+          <Image src={MailIcon} alt="mail icon" />
         </Box>
 
-        <Stack maxWidth="450px">
+        <Stack width="350px">
           <SlideFadeInOnScroll>
             <SectionHeader>
               <Stack textAlign="center">
