6fcc84913826847fc9c901b89bc2e23d109e5797
Christian <chrille_0313@hotmail.com>
2024-11-06 23:02:07 +0100

    refactor: refactor resources page with mui + styling

--- apps/ikea-photo-creator/src/app/resources/page.tsx
+++ apps/ikea-photo-creator/src/app/resources/page.tsx
@@ -1,7 +1,40 @@
-import Resources from '@/components/Resources';
-
+import SectionHeader from '@/components/Sections/SectionHeader';
+import SectionTitle from '@/components/Sections/SectionTitle';
+import { Container, Stack } from '@mui/material';
 
 export default function ResourcesPage() {
-
-    return <Resources />
+  return (
+    <Container
+      sx={{
+        paddingTop: '3rem'
+        // minHeight: '100vh'
+      }}
+    >
+      <Stack gap="2rem">
+        <SectionHeader marginBottom={0}>
+          <SectionTitle>Presentation</SectionTitle>
+        </SectionHeader>
+        <video
+          controls
+          style={{
+            borderRadius: '1rem'
+          }}
+        >
+          <source src="/static/videos/Final_Pitch_slides_19.mp4" type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+        <SectionHeader marginBottom={0}>
+          <SectionTitle>Slides</SectionTitle>
+        </SectionHeader>
+        <iframe
+          src="https://docs.google.com/presentation/d/e/2PACX-1vTpJ6KXw40Cb8MChXVFDdyJddlAMNZdcGt_nq6tfUoy5Bfq8Xv0UTZej604coJPCDjHXTxOcYc_onzC/embed?start=false&loop=false&delayms=5000"
+          style={{
+            border: 'none',
+            borderRadius: '1rem',
+            aspectRatio: '16 / 9'
+          }}
+        />
+      </Stack>
+    </Container>
+  );
 }
